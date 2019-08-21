138424211b51596c7e1cb7f47aa7fe1ab67f3640
= <=>
2019-08-21 14:43:03 -0500

    made plots

--- CMFD/iterator.py
+++ CMFD/iterator.py
@@ -1,22 +1,10 @@
 import numpy as np
-import math as math
 import matplotlib.pyplot as plt
-import os
-import sys
-from constants import fast
-from constants import thermal
-from constants import geometry
 from solver import flux
 
 N = 100  # number of gridpoints
 
-G = geometry()
-F = fast(N)
-T = thermal(N)
 flux = flux()
-
-phi_1_1 = flux.phi_1(N, flux.source_guess, flux.phi_1_guess)
-print(phi_1_1)
 
 phi_1_prev = flux.phi_1_guess
 phi_2_prev = flux.phi_2_guess
@@ -25,8 +13,9 @@ k_prev = 1.2
 convergence = False
 outer_it_num = 0
 k_evo = []
+source_evo = []
 
-while outer_it_num < 5 * 10 ** 3: # and convergence == False:
+while outer_it_num <= 5 * 10 ** 3: # and convergence == False:
     phi_1_pres = flux.phi_1(N, source_prev, phi_1_prev)
     phi_2_pres = flux.phi_2(N, phi_1_pres, phi_2_prev)
     source_pres = flux.source_update(N, phi_1_pres, phi_2_pres, k_prev)
@@ -38,9 +27,52 @@ while outer_it_num < 5 * 10 ** 3: # and convergence == False:
     phi_2_prev = phi_2_pres.copy()
     source_prev = source_pres.copy()
     k_prev = k_pres.copy()
-    # if outer_it_num % 10 ==
+    if outer_it_num % 1000 == 0:
+        source_evo.append(source_pres)
     outer_it_num += 1
 
-print(k_evo)
-plt.plot(np.arange(0, 5 * 10 ** 3), k_evo)
+def normalizer(list):
+    maximum = max(np.array(list).flatten())
+    for item in range(len(list)):
+        for subitem in range(len(list[item])):
+            list[item][subitem] /= maximum
+    return list
+
+
+# source_evo = normalizer(source_evo)
+# label_num = 0
+# for item in source_evo:
+#     plt.plot(flux.grid, item, label = str(label_num) + 'iterations')
+#     label_num += 1000
+
+# plt.xlabel('Position')
+# plt.ylabel('Source Neutron Density')
+# plt.title('Normalized Source Neutron Distribution at Different Iterations')
+# plt.legend()
+# plt.show()
+#
+# plt.plot(np.arange(0, 5 * 10 ** 3 + 1), k_evo)
+# plt.xlabel('Iteration Number')
+# plt.ylabel('k')
+# plt.title('k vs Iteration Number')
+# plt.show()
+
+
+def normalizer2(list1, list2):
+    maximum = max(max(list1), max(list2))
+    for item in range(len(list1)):
+        list1[item] /= maximum
+        list2[item] /= maximum
+    return [list1, list2]
+
+
+phi_plot = normalizer2(phi_1_pres, phi_2_pres)
+plt.plot(flux.grid, phi_plot[0], label='fast')
+plt.plot(flux.grid, phi_plot[1], label='thermal')
+plt.xlabel('Position')
+plt.ylabel('Normalized Flux')
+plt.title('Normalized and Converged Flux Distributions')
+plt.legend()
 plt.show()
+
+print(k_pres)
